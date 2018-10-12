03b88d1a0d5f9a92fc2d856c6cc161e2b658a675
Cody Kochmann <kochmanncody@gmail.com>
2018-10-12 10:38:36 -0400

    improved the updater script and added elasticsearch to the repo

--- update-builds.py
+++ update-builds.py
@@ -4,7 +4,18 @@ from contextlib import contextmanager
 
 __script_dir__ = os.path.dirname(os.path.abspath(__file__))
 
-bash = lambda c: os.popen(c).read().strip()
+def bash(command:str) -> str:
+    ''' this executes a command and streams the output to the same line '''
+    print('running:', repr(command))
+    out = []
+    previous_len = 0
+    for line in os.popen(command):
+        line = line.strip()
+        out.append(line)
+        print(line+(' '*(previous_len-len(line))), end='\r')
+        previous_len = len(line)
+    print('') # preserve the last line in the terminal
+    return '\n'.join(out).strip()
 
 class IterableQueue(Queue):
     def __iter__(self):
@@ -13,8 +24,9 @@ class IterableQueue(Queue):
                 yield self.get_nowait()
             except Empty:
                 break
+
 @contextmanager
-def pushd(d):
+def pushd(d:str):
     previous = os.getcwd()
     os.chdir(d)
     try:
@@ -22,39 +34,34 @@ def pushd(d):
     finally:
         os.chdir(previous)
 
-def is_docker_dir(i:str)->bool:
+def is_docker_dir(i:str) -> bool:
     return os.path.isdir(i) and 'Dockerfile' in os.listdir(i)
-    
-def docker_from(docker_dir:str)->str:
+
+def docker_from(docker_dir:str) -> str:
     assert is_docker_dir(docker_dir)
     with open(os.path.join(docker_dir, 'Dockerfile'), 'r') as f:
         for line in f:
             if line.startswith('FROM '):
                 return line[5:].strip()
     assert 0, 'couldnt find a FROM line for {}'.format(docker_dir)
-        
+
 with pushd(__script_dir__):
 
-    docker_dirs = {i for i in os.listdir('.') if is_docker_dir(i)}
-    
-    
+    docker_dirs = (i for i in os.listdir('.') if is_docker_dir(i))
+    updated_containers = set()
     update_q = IterableQueue()
+
     for i in docker_dirs:
         update_q.put(i)
-        
-    #print(docker_dirs)
-    
+
     for i in update_q:
-        parent_docker = docker_from(i)
-        if parent_docker.startswith('codykochmann/'):
-            parent_docker = parent_docker.split('codykochmann/')[1]
-        if parent_docker in docker_dirs:
-            update_q.put(i) # re-queue since its parent hasnt been updated yet
+        parent = docker_from(i)
+        if parent.startswith('codykochmann/') and parent.split('codykochmann/')[1] not in updated_containers:
+            update_q.put(i) # re-queue since its parent container hasnt been updated yet
         else:
-            docker_dirs.remove(i)
+            updated_containers.add(i)
             with pushd(i):
                 print('building {}...'.format(i))
                 bash('docker build --no-cache -t codykochmann/{}:latest .'.format(i))
                 print('pushing {}...'.format(i))
                 bash('docker push codykochmann/{}:latest'.format(i))
-
